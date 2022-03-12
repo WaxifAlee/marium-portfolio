23424bbabd44512ab8ca5063addbdcf49b259837
huzaifa95-star <75427330+huzaifa95-star@users.noreply.github.com>
2022-03-12 14:21:18 +0500

    improved navbar + routing
    added .map to make it clean and created components + done routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./Navbar/Navbar";
-
+import { Home, About, Health, Lifestyle, Contact } from './components';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="*" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/health" element={<Health />} />
+          <Route path="/lifestyle" element={<Lifestyle />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+      </Router>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as About } from './About'
+export { default as Contact } from './Contact';
+export { default as Health } from './Health';
+export { default as Home } from './Home';
+export { default as Lifestyle } from './Lifestyle';
